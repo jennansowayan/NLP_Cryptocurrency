6a7e750d0a2a4d14b005a02fb34ef46eecfa41dc
entaim <fnalshaikh@gmail.com>
2021-11-21 09:12:27 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ reddit = praw.Reddit(client_id='qQBQxY9R1zGrYJH9pACBOw', client_secret='MjrjCout
 
 # %%
 st.title('Crypto Trader Assistant')
-st.text('This tool gives traders and investors a sentiment about the cryptocurrency from r/Cryptocurrency.')
+st.subheader('This tool gives traders and investors a sentiment about the cryptocurrency from /n the subreddit r/Cryptocurrency.')
 
 ucurr = st.text_input('Enter a word related to the Cryptocurrency world:', 'e.g. btc')
 st.balloons()
